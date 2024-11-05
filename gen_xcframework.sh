f3368f808a7d34856a98d712cd0399cff1277a28
Valeriy Trusov <vtrusov@team.amocrm.com>
2024-11-05 16:46:35 +0300

    fix compile script

--- gen_xcframework.sh
+++ gen_xcframework.sh
@@ -10,12 +10,11 @@ set -e
 
 # Variables
 
-FRAMEWORK_NAME=$1
+FRAMEWORK_NAME="amomessenger"
 
 TEMP_BUILD_PATH="./build"
 SIMULATOR_ARCHIVE_PATH="${TEMP_BUILD_PATH}/iOS_Simulator.xcarchive"
 DEVICE_ARCHIVE_PATH="${TEMP_BUILD_PATH}/iOS.xcarchive"
-MACOS_ARCHIVE_PATH="${TEMP_BUILD_PATH}/macOS.xcarchive"
 
 OUPUT_PATH_DIR="./build"
 
@@ -24,7 +23,6 @@ echo "Framework name: ${FRAMEWORK_NAME}"
 echo "Build dir: ${TEMP_BUILD_PATH}"
 echo "Simulator arch dir: ${SIMULATOR_ARCHIVE_PATH}"
 echo "Device arch dir: ${DEVICE_ARCHIVE_PATH}"
-echo "macOS arch dir: ${MACOS_ARCHIVE_PATH}"
 
 # Remove old files
 rm -rf ${TEMP_BUILD_PATH}/*.xcarchive
@@ -40,14 +38,9 @@ xcodebuild archive -scheme ${FRAMEWORK_NAME} \
 					-destination "generic/platform=iOS Simulator" \
 					-archivePath "${SIMULATOR_ARCHIVE_PATH}"
 
-xcodebuild archive -scheme ${FRAMEWORK_NAME} \
-					-destination "generic/platform=macOS" \
-					-archivePath "${MACOS_ARCHIVE_PATH}"
-
 # XCFramework
 xcodebuild  -create-xcframework \
 			-framework ${DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-			-framework ${MACOS_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
 			-framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
 			-output ${OUPUT_PATH_DIR}/${FRAMEWORK_NAME}.xcframework
 
