01e23c53184728190e60eae7a07389e6bd56b25e
Valeriy Trusov <vtrusov@team.amocrm.com>
2024-11-28 19:45:59 +0300

    ddd

--- Package.swift
+++ Package.swift
@@ -15,4 +15,4 @@ let package = Package(
         .binaryTarget(name: "libprisma", url: "https://github.com/purkylin/libprisma/files/14610785/libprisma.zip", checksum: "11bfa012cce3628eb215386a8734978844c8a28f7e31c2c70783adaacba19981")
     ]
 )
-
+//
